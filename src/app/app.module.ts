ee76773a2ec96fd43a38f270596e67ddbc7185f4
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-06-19 16:12:53 -0300

    criando os components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { ContainerComponent } from './container/container.component';
+import { TitleComponent } from './components/title/title.component';
+import { ContentComponent } from './components/content/content.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    EventosComponent,
+    ContainerComponent,
+    TitleComponent,
+    ContentComponent,
+    HeaderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
