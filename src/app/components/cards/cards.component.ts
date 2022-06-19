4e13025799427cb46f4a9148d3d502d21af4338f
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-06-19 17:09:12 -0300

    foi feito a logica de converção das  oedas

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -11,5 +11,42 @@ export class CardsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  bitcon() {
+    let btc = document.querySelectorAll('.brl');
+    const urlBitcoin = 'https://data.messari.io/api/v1/assets/btc/metrics';
+    fetch(urlBitcoin)
+      .then(response => response.json())
+      .then(data => {
+        btc.forEach(element => {
+          element.innerHTML ='US$: ' + data.data.market_data.price_usd;
+        });
+      }
+      );
+  }
+
+  etherio() {
+    let eth = document.querySelectorAll('.brl');
+    const urlEtheruim = 'https://data.messari.io/api/v1/assets/eth/metrics';
+    fetch(urlEtheruim)
+      .then(response => response.json())
+      .then(data => {
+        eth.forEach(element => {
+          element.innerHTML = 'US$: ' + data.data.market_data.price_usd;
+        });
+      });
+  }
+
+  dolar() {
+    let us = document.querySelectorAll('.brl');
+    const urlReal = 'https://economia.awesomeapi.com.br/last/USD-BRL';
+    fetch(urlReal)
+      .then(response => response.json())
+      .then(data => {
+        us.forEach(element => {
+          element.innerHTML ="R$: " + data.USDBRL.bid;
+        });
+      });
+  }
 
 }
+
