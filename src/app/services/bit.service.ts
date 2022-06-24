2effb8a97eaf3d6ea5b91b653d319d9c6e05c3de
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-06-24 14:29:00 -0300

    reajustes

--- src/app/services/bit.service.ts
+++ src/app/services/bit.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHandler } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class BitService {
 
   constructor() { }
+
+  bitconAll() {
+
+  }
 }
